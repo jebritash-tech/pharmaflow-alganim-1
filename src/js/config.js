8ac3b9af768eb16eff03eb74364a232e8f5d8506
jebritash-tech <jebritash@gmail.com>
2026-08-27 22:40:17 +0300

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_BASE =
-    'https://pharmaflow-api-1.1.0-beta-main.test/api';
+    'https://pharmaflow-api-2-0-0-stable-alganim.onrender.com/api';
 
 export const APP_NAME =
     'PharmaFlow';
